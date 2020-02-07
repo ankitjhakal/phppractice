e349ee76d29f6e1818c338132ceda6343dffc668
ankitjhakal <aj9828791470@gmail.com>
2020-02-07 12:33:34 +0530

    rock_paper_scissor

---
+++ q9/homepage.php
@@ -0,0 +1,15 @@
+<!-- QUESTION: Make Rock, Paper, scissors using php in which one player is the user and second is computer -->
+<!-- signup form for rock paper Scissor's game -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form  action="rps.php" method="post">
+      Username: <input type="text" name="username"><br><br>
+      <input type="submit" name="submit" value="submit">
+    </form>
+  </body>
+</html>
